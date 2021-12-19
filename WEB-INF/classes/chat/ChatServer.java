9c4eb52b9eef13fc3c33d97b27f3c59f2bd6361c
mike yang <18604713262@163.com>
2021-12-19 13:58:59 -0500

    script commit

--- WEB-INF/classes/chat/ChatServer.java
+++ WEB-INF/classes/chat/ChatServer.java
@@ -37,7 +37,7 @@ public class ChatServer {
 
 
     public ChatServer() {
-        connectionIds.getAndIncreament();
+        Integer i = connectionIds.getAndIncrement();
         nickname = "";
 
     }
