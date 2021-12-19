e6a8913f70f8d6130b4aff8ee795ed436b7c0a92
mike yang <18604713262@163.com>
2021-12-19 13:57:24 -0500

    script commit

--- WEB-INF/classes/chat/ChatServer.java
+++ WEB-INF/classes/chat/ChatServer.java
@@ -15,6 +15,7 @@ import javax.websocket.server.ServerEndpoint;
 // import org.apache.juli.logging.Log;
 // import org.apache.juli.logging.LogFactory;
 import java.util.*;
+import javaserver.chat.*;
 import util.JSONParse;
 import util.HTMLFilter;
 
@@ -22,47 +23,77 @@ import util.HTMLFilter;
 public class ChatServer {
 
     // private static final Log log = LogFactory.getLog(ChatServer.class);
-
-    private static final String GUEST_PREFIX = "user_";
+    private static final String SAVE_DIR = "marshes";
+    // private static final String GUEST_PREFIX = "user_";
     private static final AtomicInteger connectionIds = new AtomicInteger(0);
     private static final Set<ChatServer> connections =
             new CopyOnWriteArraySet<>();
 
     private final String nickname;
+    public final String nickID;
     private Session session;
+    private JSONParse a_parse = new JSONParse();
+
+
 
     public ChatServer() {
-        nickname = GUEST_PREFIX + connectionIds.getAndIncrement();
+        connectionIds.getAndIncreament();
+        nickname = "";
+
     }
 
+
+    private void read_incoming (String a){
+        String appPath = request.getServletContext().getRealPath("");
+        String savePath = appPath + File.separator + SAVE_DIR;
+         
+        File fileSaveDir = new File(savePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdir();
+        }
+
+        Map<String, Object> msg = a_parse.parse(HTMLFilter.filter(a));
+        System.out.println("[chat server] new object from msg : "+ msg);
+        switch(msg.get("type")){
+            case "register":
+                System.out.println("[chat server] registration for "+msg.get("sender")+".");
+                this.nickname = msg.get(sender);
+                this.nickID = this.nickname+this.connectionIds.get().toString();
+                String regi_ack = a_parse.json_request("regi_ack","serv", this.nickname, this.nickID,"","");
+                this.send_to(this.nickID,regi_ack );
+                continue;
+            
+            default:
+                System.out.println("[chat server] Unkown type: "+msg.get("type"));
+        }
+
+    }
 
     @OnOpen
     public void start(Session session) {
         this.session = session;
         connections.add(this);
-        String message = String.format("* %s %s", nickname, "has joined.");
-        broadcast(message);
+        // String message = String.format("* %s %s", nickname, "has joined.");
+        // broadcast(message);
     }
 
 
     @OnClose
     public void end() {
         connections.remove(this);
-        String message = String.format("* %s %s",
-                nickname, "has disconnected.");
-        broadcast(message);
+        // String message = String.format("* %s %s",
+        //         nickname, "has disconnected.");
+        // broadcast(message);
     }
 
 
     @OnMessage
     public void incoming(String message) {
         // Never trust the client
-        JSONParse a = new JSONParse();
         String filteredMessage = String.format("%s: %s",
                 nickname, HTMLFilter.filter(message.toString()));
-        Map<String, Object> new_msg = a.parse(HTMLFilter.filter(message.toString()));
-        System.out.println("[chat server] new object from msg : "+ message.toString());
-        broadcast(filteredMessage);
+        
+        // broadcast(filteredMessage);
     }
 
 
@@ -70,6 +101,7 @@ public class ChatServer {
 
     @OnError
     public void onError(Throwable t) throws Throwable {
+        System.out.println("[chat server] ERROR: "+ t.toString());
         // log.error("Chat Error: " + t.toString(), t);
     }
 
@@ -92,6 +124,30 @@ public class ChatServer {
                         client.nickname, "has been disconnected.");
                 broadcast(message);
             }
+        }
+    }
+    private static void send_to(String userid, String msg) {
+        for (ChatServer client : connections) {
+            if (userid.equals(client.nickID)){
+                try {
+                    synchronized (client) {
+                        client.session.getBasicRemote().sendText(msg);
+                    }
+                } catch (IOException e) {
+                    // log.debug("Chat Error: Failed to send message to client", e);
+                    connections.remove(client);
+                    try {
+                        client.session.close();
+                    } catch (IOException e1) {
+                        // Ignore
+                    }
+                    String message = String.format("* %s %s",
+                            client.nickname, "has been disconnected.");
+                    broadcast(message);
+                }
+                return ;
+            }
+
         }
     }
 }
